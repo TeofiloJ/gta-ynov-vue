d0ca1a475eae27f0775dc2330d47ec1098033d85
Téofilo <teofilo.jeandot@gmail.com>
2018-11-22 16:53:59 +0100

    fixing ui

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -54,18 +54,65 @@
                         <b-form-input id="input-small" size="sm" type="text" placeholder="Salarié"></b-form-input>
                         </b-col>
                     </b-row>
-                    <b-row>
-                        <b-col>
-                            <b-btn>Ajouter</b-btn>
-                        </b-col>
-                    </b-row>
             </b-col>
    
             <b-col>
                 <p>Créer le contrat</p>
 
+                <table>
+                    <thead>
+                        <th>Jour</th>
+                        <th>Matin 1</th>
+                        <th>Matin 2</th>
+                        <th>Aprem 1</th>
+                        <th>Aprem 2</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>L</td>
+                            <td><input type="time" v-model="contrat.lundi.matin1"></td>
+                            <td><input type="time" v-model="contrat.lundi.matin2"></td>
+                            <td><input type="time" v-model="contrat.lundi.aprem1"></td>
+                            <td><input type="time" v-model="contrat.lundi.aprem2"></td>                            
+                        </tr>
+                        <tr>
+                            <td>M</td>
+                            <td><input type="time" v-model="contrat.mardi.matin1"></td>
+                            <td><input type="time" v-model="contrat.mardi.matin2"></td>
+                            <td><input type="time" v-model="contrat.mardi.aprem1"></td>
+                            <td><input type="time" v-model="contrat.mardi.aprem2"></td>
+                        </tr>
+                        <tr>
+                            <td>M</td>
+                            <td><input type="time" v-model="contrat.mercredi.matin1"></td>
+                            <td><input type="time" v-model="contrat.mercredi.matin2"></td>
+                            <td><input type="time" v-model="contrat.mercredi.aprem1"></td>
+                            <td><input type="time" v-model="contrat.mercredi.aprem2"></td>
+                        </tr>
+                        <tr>
+                            <td>J</td>
+                            <td><input type="time" v-model="contrat.jeudi.matin1"></td>
+                            <td><input type="time" v-model="contrat.jeudi.matin2"></td>
+                            <td><input type="time" v-model="contrat.jeudi.aprem1"></td>
+                            <td><input type="time" v-model="contrat.jeudi.aprem2"></td>
+                        </tr>
+                        <tr>
+                            <td>V</td>
+                            <td><input type="time" v-model="contrat.vendredi.matin1"></td>
+                            <td><input type="time" v-model="contrat.vendredi.matin2"></td>
+                            <td><input type="time" v-model="contrat.vendredi.aprem1"></td>
+                            <td><input type="time" v-model="contrat.vendredi.aprem2"></td>
+                        </tr>
+                    </tbody>
+                </table>
+
             </b-col>
         </b-row>
+            <b-row>
+                <b-col>
+                    <b-btn>Ajouter</b-btn>
+                </b-col>
+            </b-row>
     </b-container>
 
 </template>
@@ -77,6 +124,38 @@ export default {
   name: 'AddUser',
   data () {
     return {
+        contrat : {
+            lundi : {
+                matin1: "09:00",
+                matin2: "12:00",
+                aprem1: "13:00",
+                aprem2: "17:00",
+            },
+            mardi : {
+                matin1: "09:00",
+                matin2: "12:00",
+                aprem1: "13:00",
+                aprem2: "17:00",
+            },
+            mercredi : {
+                matin1: "09:00",
+                matin2: "12:00",
+                aprem1: "13:00",
+                aprem2: "17:00",
+            },
+            jeudi : {
+                matin1: "09:00",
+                matin2: "12:00",
+                aprem1: "13:00",
+                aprem2: "17:00",
+            },
+            vendredi : {
+                matin1: "09:00",
+                matin2: "12:00",
+                aprem1: "13:00",
+                aprem2: "17:00",
+            },
+        }
     }
   },
   methods: {
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,22 +5,25 @@
         <b-row class="m-2">
             <b-col md="4" >
                 <div class="content">
-                    <b-tabs>
-                        <b-tab title="Mon compte" active>
-                            <b-btn v-on:click="toggleComponent('DisplayBilan')">Voir mon bilan</b-btn>
-                        </b-tab>
-                        <b-tab title="Utilisateurs">
-                           <b-nav-form>
-                                <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-                                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                            </b-nav-form>
-                            
-                            <b-btn size="sm" v-on:click="toggleComponent('addUser')">Ajouter un utilisateur</b-btn>
-                        </b-tab>
-                        <b-tab title="Equipes" >
-                            <br>I'm the second tab content
-                        </b-tab>
-                    </b-tabs>
+                    <b-card no-body>
+                        <b-tabs card>
+                            <b-tab title="Mon compte" active>
+                                <b-btn v-on:click="toggleComponent('DisplayBilan')">Voir mon bilan</b-btn>
+                            </b-tab>
+                            <b-tab title="Utilisateurs">
+                            <b-nav-form>
+                                    <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+                                    <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                                </b-nav-form>
+                                
+                                <b-btn size="sm" v-on:click="toggleComponent('addUser')">Ajouter un utilisateur</b-btn>
+                            </b-tab>
+                            <b-tab title="Equipes" >
+                                <br>I'm the second tab content
+                            </b-tab>
+                        </b-tabs>
+                    </b-card>
+
                 </div>
             </b-col>
             <b-col  md="8">
